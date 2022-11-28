8102aaac78de179e2a51add99ab955d45e7e3a7a
Lara <lrpoblet@gmail.com>
2022-11-28 08:47:12 +0100

    add files proyect structure

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+//import { useStrict } from 'react';
+//import '../styles/App.scss';
+
+function App() {
+  // VARIABLES ESTADO
+
+  // USEEFFECT ?
+
+  // FUNCIONES HANDLER
+
+  // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR HTML
+
+  // HTML EN EL RETURN
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
