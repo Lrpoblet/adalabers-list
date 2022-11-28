3b23c402e561899daabdeb9b099c5fe46c43fe98
Lara <lrpoblet@gmail.com>
2022-11-28 08:55:21 +0100

    add data and html structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 //import { useStrict } from 'react';
 //import '../styles/App.scss';
+import adalabers from '../data/adalabers.json';
 
 function App() {
   // VARIABLES ESTADO
@@ -13,7 +14,36 @@ function App() {
   // HTML EN EL RETURN
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h1>Adalabers</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <!-- Primera fila --> */}
+          <tr>
+            <td>MariCarmen</td>
+            <td>Yanelis</td>
+            <td>Python</td>
+          </tr>
+          {/* <!-- Segunda fila --> */}
+          <tr>
+            <td>Amparo</td>
+            <td>Dayana</td>
+            <td>IA</td>
+          </tr>
+          {/* <!-- Tercera fila --> */}
+          <tr>
+            <td>Escandia</td>
+            <td>Iván</td>
+            <td>3D graphics</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
